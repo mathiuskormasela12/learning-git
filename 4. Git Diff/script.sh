d9d2da7b7a07dc6cbb6762414e7ba0113f6dcdea
Mathius <mathiuskormasela12@gmail.com>
2023-06-11 20:09:46 +0700

    Git Diff

---
+++ 4. Git Diff/script.sh
@@ -0,0 +1,14 @@
+# Melihat perbedaan di semua folder/file di project kita
+git diff
+
+# Melihat perbedaan di sebuah commit
+git diff commit-code
+
+# Melihat perbedaan di sebuah file atau folder
+git diff nama-file.ekstensi
+
+# Melihat perbedaan antar commit
+git diff commit-code-1 commit-code-2
+
+# Melihat perbedaan antar branch
+git diff branc1 branch2

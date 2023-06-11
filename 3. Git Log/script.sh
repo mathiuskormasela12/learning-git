b5053840e0ca6993a374304124b6ebe3dc84aa0b
Mathius <mathiuskormasela12@gmail.com>
2023-06-11 19:47:41 +0700

    Git log

---
+++ 3. Git Log/script.sh
@@ -0,0 +1,11 @@
+# Untuk melihat log history perubah pada semue file
+git log
+
+# Untuk melihat log lebih pendek (1 baris aja, jdi gk detail)
+git log --oneline
+
+# Untuk melihat log berdasarkan file/folder tertentu
+git log nama-file.ekstensi
+
+# Untuk melihat log berdasarkan kode commit tertentu commit
+git log comit-code-nya

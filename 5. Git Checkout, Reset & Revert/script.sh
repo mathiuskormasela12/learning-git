5710bd073ec13ece728ea67d7e464c7a5e913744
Mathius <mathiuskormasela12@gmail.com>
2023-06-11 20:28:21 +0700

    Git checkout revert & reset

---
+++ 5. Git Checkout, Reset & Revert/script.sh
@@ -0,0 +1,15 @@
+# Revert modified file
+git checkout nama-file.ext
+
+# Revert Staged file (akan berubah jdi modified file)
+git reset nama-file.ext
+
+# Revert commited file (akan berubah menjadi staged file)
+git checkout code-commit-nya nama-file.txt
+
+# Revert ke n commit sebelumnya
+# akan revert ke 3 commitan sebelumnya
+git checkout HEAD~3 nama-file.ext
+
+# Revert ke semua perubahan yg ada
+git revert -n code-commit-nya
